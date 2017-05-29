cbdd31ed05f1216f7c9267d99850f562a0564fcc
Juliette FRETAY <fretay.e1401371@etud.univ-ubs.fr>
2017-05-29 13:38:17 +0200

    Add Choco Dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
 import static spark.Spark.*;
 
+import org.chocosolver.solver.Model;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
 public class Main {
 
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
+        get("/test", new Route() {
+
+			@Override
+			public Object handle(Request arg0, Response arg1) throws Exception {
+				Model model = new Model("A first model");
+				return model.getName();
+			}
+        	
+        });
+        
         get("/hello", (req, res) -> "Hello Heroku World");
     }
 
